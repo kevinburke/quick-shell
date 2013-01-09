3970a643f58e7cd8037b310d50391b0c8f23da0c
Kevin Burke <kevin@twilio.com>
2013-01-08 16:52:14 -0800

    more tweaks.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 echo "set completion-ignore-case On" >> ~/.inputrc
 echo "set show-all-if-ambiguous on" >> ~/.inputrc
 echo "set completion-prefix-display-length 2" >> ~/.inputrc
-source .inputrc
+source ~/.inputrc
 
 # ack - the better grep
 PATHDIRS="
@@ -11,7 +11,7 @@ $HOME/bin
 /usr/bin"
 for dir in $PATHDIRS
 do
-    if [ echo $PATH | grep $dir; ]; then
+    if [ echo $PATH | grep $dir ]; then
         curl http://betterthangrep.com/ack-standalone > $dir/ack && chmod 0755 $dir/ack
         break
     fi
